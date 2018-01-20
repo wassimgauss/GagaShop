c0a3a5e6099d6a0502ae7d6793606fb18930876d
Wassim Tarhouni <wassim.gauss92@gmail.com>
2018-01-21 00:16:41 +0100

    PAgination valider + preparation des translations

--- src/GaussBundle/Controller/ShopController.php
+++ src/GaussBundle/Controller/ShopController.php
@@ -8,48 +8,61 @@ use Symfony\Component\HttpFoundation\Request;
 
 class ShopController extends Controller
 {
-    public function indexAction()
+    public function indexAction(Request $request)
     {
-        return $this->render('@Gauss/Shop/index.html.twig');
+        $page = $request->query->get('page');
+        return $this->render('@Gauss/Shop/index.html.twig',array('page' => $page));
     }
 
-    public function indexCategAction($id_categ){
-
-        return $this->render('@Gauss/Shop/index-categ.html.twig',array('id_categ' => $id_categ));
-    }
-
-    public function getCategoryAction(){
-        $em= $this->getDoctrine()->getManager();
-        $listCategory = $em->getRepository("GaussBundle:Category")->findAll();
-        $data = array();
-            foreach ($listCategory as $value) {
-                $listProduct = $em->getRepository("GaussBundle:Produit")->findBy(array('category' => $value));
-                array_push($data,(count($listProduct)));
-            }
-        return $this->render('@Gauss/Shop/layout/widget-categories.html.twig',array('listCategory' => $listCategory, 'count' => $data));
-    }
-
-    public function getProductCategAction($id_categ){
-        $em = $this->getDoctrine()->getManager();
-        $listProduct = $em->getRepository("GaussBundle:Produit")->findBy(array('category' => $id_categ));
-        return $this->render('@Gauss/Shop/layout/shop-body.html.twig',array('listProduct' => $listProduct));
-    }
-
-    public function getProductAction(Request $request){
-        $em = $this->getDoctrine()->getManager();
-        //$listProduct = $em->getRepository("GaussBundle:Produit")->findAll();
+    public function getProductAction(Request $request,$page){
+        if(!$page)
+            $request->query->set('page',1);
         $em    = $this->get('doctrine.orm.entity_manager');
         $dql   = "select a from GaussBundle:Produit a where a.category != 5";
         $query = $em->createQuery($dql);
         $paginator  = $this->get('knp_paginator');
         $pagination = $paginator->paginate(
             $query, /* query NOT result */
-            $request->query->getInt('page', 1)/*page number*/,
+            $request->query->get('page',$page)/*page number*/,
             6/*limit per page*/
         );
 
         return $this->render('@Gauss/Shop/layout/shop-body.html.twig',array('pagination' => $pagination));
     }
+
+    public function indexCategAction(Request $request, $id_categ){
+
+        $page = $request->query->get('page');
+        return $this->render('@Gauss/Shop/index-categ.html.twig',array('id_categ' => $id_categ, 'page' => $page));
+    }
+
+    public function getProductCategAction($id_categ, Request $request, $page){
+
+        if(!$page)
+            $request->query->set('page',1);
+        $em    = $this->get('doctrine.orm.entity_manager');
+        $dql   = "select a from GaussBundle:Produit a where a.category = ".$id_categ;
+        $query = $em->createQuery($dql);
+        $paginator  = $this->get('knp_paginator');
+        $pagination = $paginator->paginate(
+            $query, /* query NOT result */
+            $request->query->get('page',$page)/*page number*/,
+            6/*limit per page*/
+        );
+        return $this->render('@Gauss/Shop/layout/shop-body.html.twig',array('pagination' => $pagination));
+    }
+
+    public function getCategoryAction(){
+        $em= $this->getDoctrine()->getManager();
+        $listCategory = $em->getRepository("GaussBundle:Category")->findAll();
+        $data = array();
+        foreach ($listCategory as $value) {
+            $listProduct = $em->getRepository("GaussBundle:Produit")->findBy(array('category' => $value));
+            array_push($data,(count($listProduct)));
+        }
+        return $this->render('@Gauss/Shop/layout/widget-categories.html.twig',array('listCategory' => $listCategory, 'count' => $data));
+    }
+
     public function getTopSellerAction(){
         $em = $this->getDoctrine()->getManager();
         $listProductTop = $em->getRepository("GaussBundle:Produit")->findBy(array(),array('classement' => 'desc'), 4);
